08c9ba2c873192f0f7f80d34594ed5cee9c4a021
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-18 12:54:58 -0700

    Opp04.java

---
+++ Opp04.java
@@ -0,0 +1,32 @@
+
+public class Opp04 {
+	private int a,b;
+	Opp04(int k){
+		a=k;
+	
+		
+		
+		
+	}
+	public void setvalue(int num) {
+		 a=num;
+		
+		
+		
+	}
+	public int getvalue() {
+		
+		return a;
+	}
+
+	public static  void main(String[]arg) {
+		Opp04 t=new Opp04(3);
+		t.setvalue(6);
+		System.out.println(t.getvalue());
+	}
+		
+		
+		
+	
+
+}
