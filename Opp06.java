c393a8a0bad214884e4ce8d8cb8ef4e87275257e
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-19 04:55:12 -0700

    Opp06.java

---
+++ Opp06.java
@@ -0,0 +1,14 @@
+
+public class Opp06 {
+	private int s=9;
+	public  double  getarea() {
+		
+		return Math.PI*s*s;
+	}
+	public static void main(String[]arg) {
+		Opp06 t=new Opp06();
+		 System.out.println(t.getarea());
+		
+	}
+
+}
