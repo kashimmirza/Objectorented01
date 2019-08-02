da3d0f963791d5eeca1d0f142890f091e1bc8003
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-08-02 09:45:14 -0700

    Speedybicycle.java

---
+++ Speedybicycle.java
@@ -0,0 +1,16 @@
+
+public class Speedybicycle extends Bicycle {
+	  int  increment;
+	  public Speedybicycle(String colour, int ring, int paddle,int increment) {
+		  super(colour, ring, paddle);
+		  this.increment=increment;
+		  
+	  }
+	  public int getspeedy() {
+		  int speeduper=0;
+		  
+		  
+		   return speeduper+=increment;
+	  }
+
+}
