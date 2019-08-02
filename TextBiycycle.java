af81752b53c68bb12bbf7e9f8388461ee46401a5
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-08-02 09:46:35 -0700

    TextBicycle.java

---
+++ TextBiycycle.java
@@ -0,0 +1,12 @@
+
+public class TestBiycycle {
+	public static void main(String []arg) {
+		Speeddown t=new Speeddown("Red",1,2,3,5);
+		System.out.println(t.getdownspeed());
+		System.out.println(t.getspeedy());
+		System.out.println(t.colour);
+		System.out.println(t.ring);
+		
+	}
+
+}
