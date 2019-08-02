c0309ec765aecb53079d466609b00c4c9486de32
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-08-02 09:43:44 -0700

    Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,13 @@
+
+public class Bicycle {
+	 protected String colour;
+	 protected int ring;
+	 protected int paddle;
+	 Bicycle(String colour, int ring, int paddle){
+		 this.colour=colour;
+		 this.ring=ring;
+		 this.paddle=paddle;
+		 
+	 }
+
+}
