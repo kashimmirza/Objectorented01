3f02bec989576fc029a3c308c3a3347e357c0661
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-08-02 09:44:07 -0700

    Parent.java

---
+++ Parent.java
@@ -0,0 +1,15 @@
+
+public class Parent {
+	private String s;
+	Parent(String s){
+		this.s=s;
+	}
+	public void print() {
+		System.out.println(s);
+	}
+	
+	 public void Printy() {
+		 System.out.println("parent");
+	 }
+
+}
