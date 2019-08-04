615dc877025a3b35364ba9bb7470666766a9eb34
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-08-04 08:53:40 -0700

    Cours1.java

---
+++ Cours1.java
@@ -0,0 +1,16 @@
+
+public class Cours1 {
+	private String courseid;
+	private String coursetitle;
+	private int coursecredit;
+	public Cours1(String courseid, String coursetitle, int coursecredti) {
+		this.courseid=courseid;
+		this.coursetitle=coursetitle;
+		this.coursecredit=coursecredit;
+		
+	}
+	public String getcourseid() {
+		return this.courseid;
+	}
+
+}
