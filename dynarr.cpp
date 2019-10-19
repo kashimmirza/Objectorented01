7b8ec887e905ba986de00b73d25ddad29cb369a7
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-19 12:43:25 +0545

    dynarr

--- dynarr.cpp
+++ dynarr.cpp
@@ -1,53 +1,26 @@
 #include "dynarr.h"
 #include<iostream>
 using namespace std;
-
-dynarr::dynarr()
-
+template<class t>
+dynarr<t>::dynarr(int s)
 {
-    data = NULL;
-    r=0,c=0;
-}
-dynarr::dynarr(int s,int p)
-{
-    data = new int*[s];
-    for(int i=0;i<s;i++){
-        for(int j=0;j<p;j++){
-                data[i]=new int[c];
-
-
-        }
-
-    }
-}
-dynarr::~dynarr()
-{
-    delete [] data;
-}
-
-void dynarr::setvalue(int i, int j ,int v )
-{
-    data[i][j] = v;
+    data = new t[s];
+    size = s;
     //ctor
 }
-int dynarr::getvalue(int r,int c){
-  return data[r][c];
-
-}
-void dynarr::allocate (int r,int c)
+template<class t>
+dynarr<t>::~dynarr()
 {
-    data= new int*[r];
-    for(int i=0;i<r;i++){
-        data[i]=new int[c];
-    }
-
-    // data= new int[p];
-
+    delete[]data;
 }
-
-
-
-
-
+template<class t>
+t dynarr<t>:: getvalue(int dex)
+{
+    return data[dex];
+}
+template<class t>
+void dynarr<t>::setvalue(int k,t value){
+    data[k]=value;
+}
 
 
